4673cb89ca8092dd8dbc1166fac5fcb88455dee6
Ahmed-bch <ahmed.djalil.2004@gmail.com>
2025-06-13 19:29:24 +0100

    Update add_app.py

--- add_app.py
+++ add_app.py
@@ -55,7 +55,8 @@ def diagnose_and_fix_database():
         default_users = [
             ("ahmed.djalil.2004@gmail․com", "ahmed26"),
             ("bouzamaamine8@gmail.com", "amine123"),
-            ("a.a.boucherite@gmail.com","djalil26")
+            ("a.a.boucherite@gmail.com","djalil26"),
+            ("nadjibmh126@gmail.com","nadjib18")
         ]
         
         for email, password in default_users:
